09d5d765a9e077939dc5d32945606bc8d958e47f
alanblee <alanbenlee12@gmail.com>
2020-06-17 14:03:48 -0700

    single_number test ok

--- single_number/single_number.py
+++ single_number/single_number.py
@@ -1,14 +1,19 @@
-'''
+"""
 Input: a List of integers where every int except one shows up twice
 Returns: an integer
-'''
+"""
+
+
 def single_number(arr):
-    # Your code here
+    count = [0 for i in range(len(arr) + 1)]
+    for i in arr:
+        count[i] += 1
+    for i in range(len(count)):
+        if count[i] == 1:
+            return i
 
-    pass
 
-
-if __name__ == '__main__':
+if __name__ == "__main__":
     # Use the main function to test your implementation
     arr = [1, 1, 4, 4, 5, 5, 3, 3, 9, 0, 0]
 
