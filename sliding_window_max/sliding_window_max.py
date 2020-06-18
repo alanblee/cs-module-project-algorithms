f73779757574b0b087a186d1b2612883d2a8650d
alanblee <alanbenlee12@gmail.com>
2020-06-18 08:57:17 -0700

    sliding windows test ok

--- sliding_window_max/sliding_window_max.py
+++ sliding_window_max/sliding_window_max.py
@@ -1,15 +1,28 @@
-'''
+"""
 Input: a List of integers as well as an integer `k` representing the size of the sliding window
 Returns: a List of integers
-'''
-def sliding_window_max(nums, k):
+"""
+
+
+def sliding_window_max(numbers, k):
+    def get_window_max(start_idx):
+        window_max = float("-inf")
+        for i in range(start_idx, start_idx + k):
+            window_max = max(window_max, numbers[i])
+        return window_max
+
     # Your code here
+    output = []
 
-    pass
+    for i in range(len(numbers) - k + 1):
+        window_max = get_window_max(i)
+        output.append(window_max)
+
+    return output
 
 
-if __name__ == '__main__':
-    # Use the main function here to test out your implementation 
+if __name__ == "__main__":
+    # Use the main function here to test out your implementation
     arr = [1, 3, -1, -3, 5, 3, 6, 7]
     k = 3
 
